adeb7db60512a3b4a1f2348d95d08e1e794a7383
shahinalkabirmitul <mitul.cse@gmail.com>
2018-08-31 23:46:17 +0600

    Add Contact Component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+ class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Mitul</h4>
+        <ul>
+          <li>Email: mitul.cse@gmail.com</li>
+          <li>Phone: 01717785417</li>
+        </ul>
+      </div>
+    )
+  }
+}
+export default Contact;
