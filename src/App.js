e2f3187bedf0b7ca4695d5fa0b48924d73d002ab
shahinalkabirmitul <mitul.cse@gmail.com>
2018-09-01 11:43:36 +0600

    Type checking and PropTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import Header from './components/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Componenmt</h1>
-        <Contact/>
-        
+        <Header branding="Contact Manager"/>
+        <Contact name="Mitul" email="mitul.cse@gmail.com" phone="01717785417"/>
+        <Contact name="Bob" email="bob.cse@gmail.com" phone="888-5656-4646"/>
       </div>
     );
   }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
  class Contact extends Component {
   render() {
+    const {name,email,phone} = this.props;
     return (
       <div>
-        <h4>Mitul</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>Email: mitul.cse@gmail.com</li>
-          <li>Phone: 01717785417</li>
+          <li>Email : {email}</li>
+          <li>Phone : {phone}</li>
         </ul>
       </div>
     )
   }
+}
+Contact.propTypes= {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
 }
 export default Contact;
