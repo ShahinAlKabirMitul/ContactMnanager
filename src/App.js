bb31becf563846956d23eb63ebf3f55f7abb6809
shahinalkabirmitul <mitul.cse@gmail.com>
2018-09-01 15:29:27 +0600

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
@@ -7,8 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager"/>
-        <Contact name="Mitul" email="mitul.cse@gmail.com" phone="01717785417"/>
-        <Contact name="Bob" email="bob.cse@gmail.com" phone="888-5656-4646"/>
+        <div className="containter">
+          <Contact name="Mitul" email="mitul.cse@gmail.com" phone="01717785417"/>
+          <Contact name="Bob" email="bob.cse@gmail.com" phone="888-5656-4646"/>
+        </div>
+        
       </div>
     );
   }
