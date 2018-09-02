c33784fda2fa133def6fd6763df2d336c53aa292
shahinalkabirmitul <mitul.cse@gmail.com>
2018-09-02 23:51:05 +0600

    toggle contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  }
+  onShowDown = e => {
+    this.setState({showContactInfo:!this.state.showContactInfo})
+  };
   render() {
-    const {name,email,phone} = this.props.contact;
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email : {email}</li>
-          <li className="list-group-item">Phone : {phone}</li>
-        </ul>
+        <h4>
+          {name} <i onClick={this.onShowDown} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo &&
+          <ul className="list-group">
+            <li className="list-group-item">Email : {email}</li>
+            <li className="list-group-item">Phone : {phone}</li>
+          </ul>
+        }
       </div>
-    )
+    );
   }
 }
-Contact.propTypes= {
-  contact: PropTypes.object.isRequired,
-}
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
 export default Contact;
